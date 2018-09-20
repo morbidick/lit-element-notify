c736a08692850ce78e79f89c6302027abfb20ade
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2018-09-20 10:26:20 +0200

    rewrite to use LitElements createProperty call, fixes #2

--- lit-element-notify.js
+++ lit-element-notify.js
@@ -1,22 +1,30 @@
 export const notify = (baseElement) => class extends baseElement {
-    constructor() {
-        super();
+    /**
+     * Extend the LitElement `createProperty` method to map properties to events
+     */
+    static createProperty(name, options) {
+        super.createProperty(name, options);
 
-        this._propertyEventMap = new Map();
-        const props = this.constructor.properties;
-      
-        for (const prop of Object.getOwnPropertyNames(props)) {
-            const config = props[prop];
-            if (config.notify) {
-                this._propertyEventMap.set(prop, this.constructor._eventName(prop, config));
-            }
+        if (!this.hasOwnProperty('_propertyEventMap')) {
+            this._propertyEventMap = new Map();
+        }
+
+        if (options.notify) {
+            this._propertyEventMap.set(name, this._eventNameForProperty(name, options));
         }
     }
 
+    /**
+     * check for changed properties with notify option and fire the events
+     */
     update(props) {
         super.update(props);
 
-        for (const [eventProp, eventName] of this._propertyEventMap.entries()) {
+        if (!this.constructor._propertyEventMap) {
+            return;
+        }
+
+        for (const [eventProp, eventName] of this.constructor._propertyEventMap.entries()) {
             if (props.has(eventProp)) {
                 this.dispatchEvent(new CustomEvent(eventName, {
                     detail: {
@@ -29,14 +37,17 @@ export const notify = (baseElement) => class extends baseElement {
         }
     }
 
-    static _eventName(property, config) {
-        if (config.notify && typeof config.notify === 'string')
-            return config.notify;
+    /**
+     * Returns the event name for the given property.
+     */
+    static _eventNameForProperty(name, options) {
+        if (options.notify && typeof options.notify === 'string')
+            return options.notify;
 
-        if (config.attribute && typeof config.attribute === 'string')
-            return `${config.attribute}-changed`;
+        if (options.attribute && typeof options.attribute === 'string')
+            return `${options.attribute}-changed`;
 
-        return `${property.toLowerCase()}-changed`;
+        return `${name.toLowerCase()}-changed`;
     }
 };
 

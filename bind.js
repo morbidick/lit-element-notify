c4846d2b07b3aabad2d4f30ee7d04630f3847cc8
Lukas Mohrbacher <lukas.mohrbacher@diconium.com>
2018-10-23 15:33:00 +0200

    implement bind directive

---
+++ bind.js
@@ -0,0 +1,26 @@
+import {directive} from "lit-html/lib/directive.js";
+import {eventNameForProperty} from "./lit-element-notify.js";
+
+/**
+ * lit-html directive to bind an element property to a sibling property
+ *
+ * @param {HTMLElement} element - The element to update
+ * @param {string} property - The property to bind to
+ * @param {string} [eventName] - Optional event name to bind to, defaults to *-changed
+ */
+export const bind = (element, property, eventName) => directive((part) => {
+    part.setValue(element[property]);
+
+    // mark the committer so the listener is only attached once
+    if (!part.committer.bindInitialized) {
+        part.committer.bindInitialized = true;
+
+        const notifyingElement = part.committer.element;
+        const notifyingProperty = part.committer.name;
+        const notifyingEvent = eventName || eventNameForProperty(notifyingProperty);
+
+        notifyingElement.addEventListener(notifyingEvent, () => {
+            element[property] = notifyingElement[notifyingProperty];
+        });
+    }
+});

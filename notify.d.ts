100a85530d3016f4845a0fa5c1497d80323fafe7
Benny Powers <bennypowers@users.noreply.github.com>
2020-04-21 10:13:36 +0300

    Update notify.d.ts
    Way better typings this way.

--- notify.d.ts
+++ notify.d.ts
@@ -1,14 +1,12 @@
-import { PropertyDeclaration, LitElement, UpdatingElement } from 'lit-element';
+import { LitElement, UpdatingElement } from 'lit-element';
 
 type Constructor<T = LitElement> = new (...args: any[]) => T;
 
-interface AugmentedPropertyDeclaration extends PropertyDeclaration {
-  /** When true will notify. Pass a string to define the event name to fire. */
-  notify: string|Boolean
+declare module 'lit-element/lib/updating-element' {
+  export interface PropertyDeclaration {
+    /** When true will notify. Pass a string to define the event name to fire. */
+    notify?: string|Boolean
+  }
 }
 
-declare class NotifyingElement {
-  static createProperty(name: string, options: AugmentedPropertyDeclaration): void
-}
-
-export function LitNotify<T extends UpdatingElement>(baseElement: Constructor<T>): T & NotifyingElement
+export function LitNotify<T extends Constructor<UpdatingElement>>(baseElement: T): T
